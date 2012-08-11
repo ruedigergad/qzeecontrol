68b623c0e3803b5cbc72eb82a5c5786cd196a047
Ruediger Gad <r.c.g@gmx.de>
2012-08-11 18:32:07 +0200

    Use absolute path to main.qml.

--- main.cpp
+++ main.cpp
@@ -32,7 +32,7 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
 
     QmlApplicationViewer viewer;
     viewer.setOrientation(QmlApplicationViewer::ScreenOrientationAuto);
-    viewer.setMainQmlFile(QLatin1String("qml/QZeeControl/main.qml"));
+    viewer.setMainQmlFile(QLatin1String("/opt/QZeeControl/qml/QZeeControl/main.qml"));
     viewer.showExpanded();
 
     return app->exec();
